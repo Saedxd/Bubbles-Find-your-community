605d3f3067545a74df418b76025f2197af651168
saed younis <saedyounis99@gmail.com>
2022-06-14 22:56:05 +0300

    fixes

--- lib/main.dart
+++ lib/main.dart
@@ -70,6 +70,20 @@ try {
 // E0A41E
 // 31576D
 // 8D4624
-
+//…or create a new repository on the command line
+//
+// echo "# Bubbles" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Saedxd/Bubbles.git
+// git push -u origin main
+//
+// …or push an existing repository from the command line
+//
+// git remote add origin https://github.com/Saedxd/Bubbles.git
+// git branch -M main
+// git push -u origin main
 
 
